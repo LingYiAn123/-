c40a4fe704d69dd396ce29ba294807f79852cdb9
renfangxiao <3422733823@qq.com>
2026-05-21 14:19:09 +0800

    初始化代码

---
+++ 准备.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std;
+#define ve vector
+#define all(x) x.begin(),x.end()
+#define ll long long
+void solves()
+{
+    
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solves();
+    }
+    return 0;
+}
